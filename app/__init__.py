a5e0e090a6965544c629896b7369b4de19275c0c
GayathriManeksha <gaya3maneksha@github.com>
2023-03-17 22:25:23 +0530

    mongodb variable

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,9 @@ import os
 
 # os.environ['MONGO_URI'] = 'mongodb+srv://GayathriManeksha:Gaya3@cluster0.anlc4cj.mongodb.net/?retryWrites=true&w=majority'
 # os.environ['MONGO_DB_NAME'] = 'mydatabase'
+mongodb_password = os.environ.get('MONGODB_PASSWORD')
+print("HEY____________________________")
+print(mongodb_password)
 
 client = MongoClient("mongodb+srv://GayathriManeksha:Gaya3@cluster0.anlc4cj.mongodb.net/?retryWrites=true&w=majority")
 db = client.mydatabase
